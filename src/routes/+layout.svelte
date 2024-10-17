f6bd0ef15f5037a4609aef2c5f7a7cceb97b8589
sai <saitheninja@gmail.com>
2024-10-17 13:08:12 +0200

    clean up root +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,17 @@
 <script>
   import Header from "./Header.svelte";
+
   import "../app.css";
 </script>
 
-<div class="app">
-  <Header></Header>
+<div class="flex min-h-screen flex-col">
+  <Header />
 
-  <main>
-    <slot></slot>
+  <main class="m-auto flex h-full w-full max-w-5xl flex-grow flex-col p-4">
+    <slot />
   </main>
 
-  <footer>
-    <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+  <footer class="flex flex-col items-center justify-center p-2 text-sm italic">
+    <p>This is a footer.</p>
   </footer>
 </div>
-
-<style>
-  .app {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
-
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    padding: 1rem;
-    width: 100%;
-    max-width: 64rem;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-
-  footer {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 12px;
-  }
-
-  footer a {
-    font-weight: bold;
-  }
-
-  @media (min-width: 480px) {
-    footer {
-      padding: 12px 0;
-    }
-  }
-</style>
