88cdf9dba1a5a3a00b00d4bc45332a564543bac4
sai <saitheninja@gmail.com>
2024-11-07 04:54:55 +0200

    add "About" info to home page, and update README

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Scryfall API Demo
 
 This is a demo project built with SvelteKit (like Next.js for Svelte).
-It has a demo login that saves data to the browser's local storage.
-It uses data from [Scryfall](https://scryfall.com/docs/api) to generate some D3 charts.
+It has a demo log in that saves data to the browser's local storage.
+It uses data from [the Scryfall API](https://scryfall.com/docs/api) to generate D3 charts and an HTML table.
 
 ## Run
 
@@ -22,15 +22,22 @@ or
 
 ### `/`
 
-Has `LogInForm` component. Will be redirected here if no log in data is in local storage.
+Home page.
 
-### `/data`
+### `/log-in`
 
-Data from [Scryfall](https://scryfall.com/docs/api/bulk-data), hosted locally as a REST endpoint.
+Has `LogInForm` component that sends a request to a REST endpoint.
+Displays error message on failure.
+On success, saves log in details to browser's local storage.
 
 ### `/scryfall`
 
 UI to examine data.
-Select range of cards.
+Select range of cards to fetch from endpoint.
 Generate bar chart of CMCs.
 Generate pie chart of color identities.
+Generate table of relevant data.
+
+### `/data`
+
+Data from [the Scryfall Bulk Data API](https://scryfall.com/docs/api/bulk-data), hosted locally as a REST endpoint.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,6 +18,15 @@
     <!-- <p class="text-minor"></p> -->
   </hgroup>
 
+  <p>This is a demo project built with SvelteKit (like Next.js for Svelte).</p>
+
+  <p>It has a demo log in that saves data to the browser's local storage.</p>
+
+  <p>
+    It uses data from <a href="https://scryfall.com/docs/api">the Scryfall API</a> to generate D3 charts
+    and an HTML table.
+  </p>
+
   {#if $jwt !== "loggedin"}
     <p><a href="/log-in">Log in to view the data.</a></p>
   {:else}
@@ -25,4 +34,42 @@
       You can now access the <a href="/scryfall">Scryfall data page</a>.
     </p>
   {/if}
+
+  <div class="mt-12 space-y-4">
+    <h2 class="heading-style-2">Routes</h2>
+
+    <div>
+      <h3 class="heading-style-3 font-mono">/</h3>
+
+      <p>Home page.</p>
+    </div>
+
+    <div>
+      <h3 class="heading-style-3 font-mono">/log-in</h3>
+
+      <p>Has LogInForm component that sends a request to a REST endpoint.</p>
+      <p>Displays error message on failure.</p>
+      <p>On success, saves log in details to browser's local storage.</p>
+    </div>
+
+    <div>
+      <h3 class="heading-style-3 font-mono">/scryfall</h3>
+
+      <p>UI to examine data.</p>
+      <p>Select range of cards to fetch from endpoint.</p>
+
+      <p>Generate bar chart of CMCs.</p>
+      <p>Generate pie chart of color identities.</p>
+      <p>Generate table of relevant data.</p>
+    </div>
+
+    <div>
+      <h3 class="heading-style-3 font-mono">/data</h3>
+
+      <p>
+        Data from <a href="https://scryfall.com/docs/api/bulk-data">the Scryfall Bulk Data API</a>,
+        hosted locally as a REST endpoint.
+      </p>
+    </div>
+  </div>
 </section>
